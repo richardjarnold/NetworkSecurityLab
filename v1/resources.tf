20da1c7c1fbe074db37281c5485de2f706caeaaf
richardjarnold <richard.arnold@inmar.com>
2025-03-26 09:36:41 -0400

    Small fix

--- v1/resources.tf
+++ v1/resources.tf
@@ -11,11 +11,11 @@ resource "aws_key_pair" "lab_key" {
 # Deploy a public EC2 instance
 resource "aws_instance" "lab_public_instance" {
   ami                    = "ami-04aa00acb1165b32a"
-  instance_type          = "t2.micro" 
+  instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = aws_key_pair.lab_key.key_name
   vpc_security_group_ids = [aws_security_group.lab_public_sg.id]
-
+  
   tags = {
     Name = "Lab-Instance-Public"
   }
@@ -24,7 +24,7 @@ resource "aws_instance" "lab_public_instance" {
 # Deploy a private EC2 instance
 resource "aws_instance" "lab_private_instance" {
   ami                    = "ami-04aa00acb1165b32a"
-  instance_type          = "t2.micro" 
+  instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private_subnet.id
   key_name               = aws_key_pair.lab_key.key_name
   vpc_security_group_ids = [aws_security_group.lab_private_sg.id]
