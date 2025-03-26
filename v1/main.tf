443b5e5b9599c9b3ac2033d5c020811bb9706416
richardjarnold <richard.arnold@inmar.com>
2025-03-26 09:35:52 -0400

    Fixed SSH between Public and Private

--- v1/main.tf
+++ v1/main.tf
@@ -1,6 +1,6 @@
 # Define the provider and the region to use
 provider "aws" {
-  region = "us-east-1"  # AWS Region
+  region = "us-east-1" # AWS Region
 }
 
 # Create VPC
@@ -26,9 +26,9 @@ resource "aws_subnet" "public_subnet" {
 
 # Create a private subnet
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.lab_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  vpc_id            = aws_vpc.lab_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "Lab-Subnet-Private"
@@ -44,7 +44,7 @@ resource "aws_internet_gateway" "lab_igw" {
   }
 }
 
-# Create a Route Table
+# Create a Route Table for public
 resource "aws_route_table" "lab_public_rt" {
   vpc_id = aws_vpc.lab_vpc.id
 
@@ -62,6 +62,21 @@ resource "aws_route_table" "lab_public_rt" {
 resource "aws_route_table_association" "lab_public_rt_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.lab_public_rt.id
+}
+
+# Create a Route Table for the Private Subnet
+resource "aws_route_table" "lab_private_rt" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name = "Lab-RT-Private"
+  }
+}
+
+# Associate the Private Route Table with the Private Subnet
+resource "aws_route_table_association" "lab_private_rt_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.lab_private_rt.id
 }
 
 # Security Group for Public
@@ -95,7 +110,7 @@ resource "aws_security_group" "lab_public_sg" {
 resource "aws_security_group" "lab_private_sg" {
   name        = "Lab-Private-SG"
   description = "Allow traffic between private and public"
-  vpc_id      = aws_vpc.lab_vpc.id  
+  vpc_id      = aws_vpc.lab_vpc.id
 
   ingress {
     description = "Allow traffic between private and public (not internet)"
@@ -127,6 +142,7 @@ resource "aws_network_acl" "public_nacl" {
   }
 }
 
+# Inbound SSH from anywhere
 resource "aws_network_acl_rule" "public_inbound_ssh" {
   network_acl_id = aws_network_acl.public_nacl.id
   rule_number    = 100
@@ -138,6 +154,19 @@ resource "aws_network_acl_rule" "public_inbound_ssh" {
   to_port        = 22
 }
 
+# Inbound ephemeral ports (for return traffic)
+resource "aws_network_acl_rule" "public_inbound_ephemeral" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+# Outbound all traffic
 resource "aws_network_acl_rule" "public_outbound_all" {
   network_acl_id = aws_network_acl.public_nacl.id
   rule_number    = 100
@@ -149,6 +178,7 @@ resource "aws_network_acl_rule" "public_outbound_all" {
   to_port        = 0
 }
 
+# Associate Public NACL with Public Subnet
 resource "aws_network_acl_association" "public_nacl_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   network_acl_id = aws_network_acl.public_nacl.id
@@ -163,6 +193,7 @@ resource "aws_network_acl" "private_nacl" {
   }
 }
 
+# Inbound from public subnet (all ports for simplicity)
 resource "aws_network_acl_rule" "private_inbound_from_public" {
   network_acl_id = aws_network_acl.private_nacl.id
   rule_number    = 100
@@ -171,9 +202,22 @@ resource "aws_network_acl_rule" "private_inbound_from_public" {
   rule_action    = "allow"
   cidr_block     = "10.0.1.0/24"
   from_port      = 0
-  to_port        = 0
+  to_port        = 65535
 }
 
+# Inbound ephemeral (for return SSH or other TCP responses)
+resource "aws_network_acl_rule" "private_inbound_ephemeral" {
+  network_acl_id = aws_network_acl.private_nacl.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "10.0.1.0/24"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+# Outbound to public subnet
 resource "aws_network_acl_rule" "private_outbound_to_public" {
   network_acl_id = aws_network_acl.private_nacl.id
   rule_number    = 100
@@ -182,9 +226,22 @@ resource "aws_network_acl_rule" "private_outbound_to_public" {
   rule_action    = "allow"
   cidr_block     = "10.0.1.0/24"
   from_port      = 0
-  to_port        = 0
+  to_port        = 65535
 }
 
+# Outbound ephemeral ports
+resource "aws_network_acl_rule" "private_outbound_ephemeral" {
+  network_acl_id = aws_network_acl.private_nacl.id
+  rule_number    = 110
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "10.0.1.0/24"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+# Associate Private NACL with Private Subnet
 resource "aws_network_acl_association" "private_nacl_assoc" {
   subnet_id      = aws_subnet.private_subnet.id
   network_acl_id = aws_network_acl.private_nacl.id
