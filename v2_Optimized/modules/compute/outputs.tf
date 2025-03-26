96525816d3835158b01a56ae7c1d0e506486acc2
richardjarnold <richard.arnold@inmar.com>
2025-03-26 10:00:02 -0400

    Fixed outputs

--- v2_Optimized/modules/compute/outputs.tf
+++ v2_Optimized/modules/compute/outputs.tf
@@ -2,3 +2,8 @@ output "public_instance_ip" {
   description = "Public IP of the public EC2 instance"
   value       = aws_instance.lab_public_instance.public_ip
 }
+
+output "private_instance_ip" {
+  description = "Private IP of the private EC2 instance"
+  value       = aws_instance.lab_private_instance.private_ip
+}
