8cf9fe1fa596654266d425eb002e728267e38e1b
richardjarnold <richard.arnold@inmar.com>
2025-03-26 09:36:12 -0400

    Set to output both private and public for readability

--- v1/outputs.tf
+++ v1/outputs.tf
@@ -3,3 +3,9 @@ output "public_instance_ip" {
   description = "Public IP of the public EC2 instance"
   value       = aws_instance.lab_public_instance.public_ip
 }
+
+# Output the private IP of the private EC2 instance
+output "private_instance_ip" {
+  description = "Private IP of the private EC2 instance"
+  value       = aws_instance.lab_private_instance.private_ip
+}
